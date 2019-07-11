10b0ed7ae64a08843896900fc45669edf941f839
harisfeb19 <harisfeb19.gmail.com>
2019-07-11 16:00:55 +0500

    First

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into Employee values(101,'Software Engineer','Haris');
+insert into Employee values(102,'SQA','Ali');
+insert into Employee values(103,'Web Engineer','Hamza');
+insert into Employee values(104,'DevOps','Fawad');
